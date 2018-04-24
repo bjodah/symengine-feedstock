3c1e62879cd5e20cdd3a65606d6d89ed9b827b19
Björn Dahlgren <bjodah@gmail.com>
2018-04-24 04:00:33 +0200

    ldd on libsymengine.so says libc++abi.so.1 => not found

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,16 +5,19 @@ cd build
 
 export CXX=clang++
 export CC=clang-6.0
+export CFLAGS=-stdlib=libc++
 
 cmake \
+    -DCMAKE_BUILD_TYPE:STRING="Release" \
     -DCMAKE_CXX_FLAGS_RELEASE="-O2 -stdlib=libc++" \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
+    -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
     -DBUILD_BENCHMARKS=no \
     -DINTEGER_CLASS=gmp \
     -DWITH_SYMENGINE_THREAD_SAFE=yes \
     -DWITH_MPC=yes \
-    -DBUILD_FOR_DISTRIBUTION=yes \
     -DBUILD_SHARED_LIBS=yes \
     -DWITH_LLVM=yes \
     -DCMAKE_INSTALL_LIBDIR=lib \
